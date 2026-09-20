181029febcf474c81cbea4153b82eddcb1447422
Joaquin Freire <ernestojoaquinfreire@gmail.com>
2026-09-20 04:21:57 -0300

    chore: identify deployed bootstrap version

--- start.cjs
+++ start.cjs
@@ -1,5 +1,6 @@
 const http = require('http')
 const port = process.env.PORT || process.env.SERVER_PORT || 3000
+console.log('[ JOA-KING ] Bootstrap e2b677a: sin acceso a stdin')
 
 global.__joaBootstrapServer = http.createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' })
